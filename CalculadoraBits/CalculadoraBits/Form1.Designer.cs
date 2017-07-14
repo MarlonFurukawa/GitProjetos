269cfc3000a89568568d6943c771e1e05c65c73d
Marlon Furukawa <marlondossantosfurukawa@yahoo.com.br>
2017-07-14 17:01:08 -0300

    CalculadoraBits

---
+++ CalculadoraBits/CalculadoraBits/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CalculadoraBits
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.lbTamanho = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtString
+            // 
+            this.txtString.Location = new System.Drawing.Point(53, 79);
+            this.txtString.Name = "txtString";
+            this.txtString.Size = new System.Drawing.Size(100, 20);
+            this.txtString.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Texto:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(53, 139);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.AutoSize = true;
+            this.lbResultado.Location = new System.Drawing.Point(50, 230);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(55, 13);
+            this.lbResultado.TabIndex = 3;
+            this.lbResultado.Text = "Resultado";
+            // 
+            // lbTamanho
+            // 
+            this.lbTamanho.AutoSize = true;
+            this.lbTamanho.Location = new System.Drawing.Point(50, 177);
+            this.lbTamanho.Name = "lbTamanho";
+            this.lbTamanho.Size = new System.Drawing.Size(82, 13);
+            this.lbTamanho.TabIndex = 4;
+            this.lbTamanho.Text = "Tamanho String";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lbTamanho);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtString);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lbResultado;
+        private System.Windows.Forms.Label lbTamanho;
+    }
+}
+
---
+++ CalculadoraBits/CalculadoraBits/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraBits
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            var Texto = txtString.Text;
+            GetBits(Texto);
+        }
+        public string GetBits(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in Encoding.Unicode.GetBytes(input))
+            {
+                sb.Append(Convert.ToString(b, 2));
+            }
+
+            var Tamanho = sb.Length;
+            lbTamanho.Text = Tamanho.ToString();
+
+            var result = Convert.ToString(sb.Length * 32);
+            lbResultado.Text = result;
+            //MessageBox.Show(result);
+            return sb.ToString();
+        }
+    }
+}
